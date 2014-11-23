23489b7dea345f9ffb83210aac640f572d7d4024
Dmitriy Ugnichenko <mitya@mityaz.co.ua>
2014-11-23 17:04:59 +0200

    Save selected options and restore on startup.

--- src/options.js
+++ src/options.js
@@ -1,38 +1,42 @@
 // Saves options to chrome.storage
 function save_options() {
   var mapProviders = document.getElementsByTagName("input");
-  var checked = [];
+  var checkedValues = [];
   for (var i = 0; i < mapProviders.length; i++) {
     if (mapProviders[i].type == "checkbox" && mapProviders[i].checked) {
-        checked.push(mapProviders[i]);
+        checkedValues.push(mapProviders[i].value);
     }
   }
   chrome.storage.sync.set({
-    mapProviders: mapProviders
+    mapProviders: checkedValues
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    status.textContent = 'Options saved!';
     setTimeout(function() {
       status.textContent = '';
-    }, 750);
+    }, 1750);
   });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  chrome.storage.sync.get({
-    mapProviders: []
-  }, function(items) {
-    mapProviders = document.getElementsByTagName("input");
-    for (var i = 0; i < mapProviders.length; i++) {
-      if (mapProviders[i].type == "checkbox") {
-
+  chrome.storage.sync.get(
+    'mapProviders', function(settings) {
+      var mapProviders = document.getElementsByTagName("input");
+      for (var i = 0; i < settings.mapProviders.length; i++) {
+        for (var j = 0; j < mapProviders.length; j++) {
+          debugger;
+          if (mapProviders[j].type == "checkbox" &&
+              mapProviders[j].value == settings.mapProviders[i]) {
+                mapProviders[j].checked = true;
+                break;
+          }
+        }
       }
     }
-    document.getElementById('provider').checked = items.mapProviders;
-  });
+  );
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
